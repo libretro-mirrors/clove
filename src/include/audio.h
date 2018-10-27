e3fd241c45714e1a489eb6a24f8ac5eb86350c1d
Murii <vmuresan@vlad-t440p>
2018-10-27 13:19:02 +0300

    Misc

--- src/include/audio.h
+++ src/include/audio.h
@@ -1,14 +1,14 @@
 /*
 #   clove
 #
-#   Copyright (C) 2016-2017 Muresan Vlad
+#   Copyright (C) 2016-2018 Muresan Vlad
 #
 #   This project is free software; you can redistribute it and/or modify it
 #   under the terms of the MIT license. See LICENSE.md for details.
 */
 #pragma once
 
-#include <AL/al.h>//"../3rdparty/openal/include/AL/al.h"
+#include "../3rdparty/openal/include/AL/al.h"
 #include "stb_vorbis.h"
 #include "streamsource.h"
 
