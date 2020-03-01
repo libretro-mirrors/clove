67c29e6b80bbc4131ea88a9c4725fbfaaa1b49fd
MuresanVlad <vmuresan@computervoice.ro>
2020-03-01 12:56:55 +0200

    forgot to add #pargma once in particlesystem.h

--- src/include/particlesystem.h
+++ src/include/particlesystem.h
@@ -1,11 +1,13 @@
 /*
 #   clove
 #
-#   Copyright (C) 2018 Muresan Vlad
+#   Copyright (C) 2019 Muresan Vlad
 #
 #   This project is free software; you can redistribute it and/or modify it
 #   under the terms of the MIT license. See LICENSE.md for details.
 */
+
+#pragma once
 
 #include "graphics.h"
 #include "batch.h"
